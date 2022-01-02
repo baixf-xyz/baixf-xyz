b272ed450b486121e8cd8322a2cc5180f46af9dd
baixf-xyz <707401057@qq.com>
2022-01-02 19:15:01 +0800

    0102

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
      * 改用 JsonBird：https://bird.ioliu.cn/
      *https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8
      */
-    var url = 'https://api.ioliu.cn/bing/';
+    var url = 'https://bing.ioliu.cn/';
     var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
     var index = sessionStorage.getItem("index");
     var $panel = $('#panel');
